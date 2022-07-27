e8fc2a47748519f880ed839e72c98b99502d34d5
Edmund Debrah <edmundking05@gmail.com>
2022-07-27 18:58:12 +0000

    add functionality to mobile menu

---
+++ index.js
@@ -0,0 +1,26 @@
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const navLink = document.getElementsByClassName(".nav-link");
+
+navToggle.addEventListener('click', () => {
+  const visible = primaryNav.getAttribute('data-visible');
+
+  if (visible === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.innerText = "x";
+    navToggle.style.position = "fixed";
+  } 
+
+  else if (visible === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.innerText = "";
+    navToggle.style.position = "absolute";
+  }
+})
+
+navLink.addEventListener('click', () => {
+  primaryNav.toggleAttribute('data-visible');
+
+}
+)
+ 
