8ec89bb76607cce5e6d67b064e92a0a564a4f095
Edmund Debrah <edmundking05@gmail.com>
2022-07-27 21:18:16 +0000

    fix linter error

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const primaryNav = document.querySelector(".primary-navigation");
-const navToggle = document.querySelector(".mobile-nav-toggle");
-const navLink = document.getElementsByClassName(".nav-link");
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const navLink = document.getElementsByClassName('.nav-link');
 
 navToggle.addEventListener('click', () => {
   const visible = primaryNav.getAttribute('data-visible');
 
-  if (visible === "false") {
-    primaryNav.setAttribute("data-visible", true);
-    navToggle.innerText = "x";
-    navToggle.style.position = "fixed";
-  } 
+  if (visible === 'false') {
+    primaryNav.setAttribute('data-visible', true);
+    navToggle.innerText = 'x';
+    navToggle.style.position = 'fixed';
+  }
 
-  else if (visible === "true") {
-    primaryNav.setAttribute("data-visible", false);
-    navToggle.innerText = "";
-    navToggle.style.position = "absolute";
+  else if (visible === 'true') {
+    primaryNav.setAttribute('data-visible', false);
+    navToggle.innerText = '';
+    navToggle.style.position = 'absolute';
   }
 })
 
 navLink.addEventListener('click', () => {
   primaryNav.toggleAttribute('data-visible');
-
 }
 )
- 
